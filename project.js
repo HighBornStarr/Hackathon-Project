72ed390e3efe2e89c642476615f532206125727e
Alex Starr <116136090+HighBornStarr@users.noreply.github.com>
2022-11-27 15:28:31 -0600

    Cards complete

--- project.js
+++ project.js
@@ -1,4 +1,7 @@
 // use .get to pull info from API
+//declare person variables from person classes
+
+
 $.get("https://randomuser.me/api/?results=3", (data) => {
   let $person0 = $("#person0");
   let $person1 = $("#person1");
@@ -8,8 +11,8 @@ $.get("https://randomuser.me/api/?results=3", (data) => {
   const randomPerson2 = data.results[2];
   // DOM Manipulation to gather image
   $person0.attr("src", randomPerson0.picture.large);
-  $person1.attr("src", randomPerson0.picture.large);
-  $person2.attr("src", randomPerson0.picture.large);
+  $person1.attr("src", randomPerson1.picture.large);
+  $person2.attr("src", randomPerson2.picture.large);
 
   //Add random instructor name 1
   $name0 = $("#name0");
@@ -17,10 +20,15 @@ $.get("https://randomuser.me/api/?results=3", (data) => {
 
 
   //Instructor name 2
+  $name1 = $("#name1");
+  $name1.text(randomPerson1.name.first);
 
 
 
   //Instructor name 3
+  $name2 = $("#name2");
+  $name2.text(randomPerson2.name.first);
+
 });
 
 /* 
